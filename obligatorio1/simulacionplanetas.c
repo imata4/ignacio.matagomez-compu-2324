c3aec02201c2dca8de00bdd2c4c220020cc38984
ignacio.matagomez <ignaciomatagomez4@gmail.com>
2024-04-03 16:33:25 +0200

    cambios repo

---
+++ obligatorio1/simulacionplanetas.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+//Declaración de funciones a utilizar
+
+
+main()
+{
+    double t; //Tiempo
+    double h; 
+    double m[5]; //masa
+    double r[5]; //posiciones
+    double v[5]; // velocidades 
+
+    FILE *fe, *fs; // Punteros para ficheros de entrada y salida
+
+
+    fe=fopen("masa.txt");
+    
+
+
+
+
+
+
+
+
+
+
+
+}
