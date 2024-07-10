a989bffdaa8a32f590b6dd86822d63b3233fe542
ignacio.matagomez <ignaciomatagomez4@gmail.com>
2024-07-10 16:45:38 +0200

    h=0.001

--- voluntario2/pendulodefinitivo.c
+++ voluntario2/pendulodefinitivo.c
@@ -36,7 +36,7 @@ int main(void)
     f4=fopen("lyapunov E=1.txt", "w");
 
     //Establecemos las variables que regulan el tiempo de simulación
-    h=0.00001;
+    h=0.0001;
     tf=100;
     t=0;
     //Condiciones iniciales
